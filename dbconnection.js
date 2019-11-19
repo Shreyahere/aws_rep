8a6a1833db42f9e1cbbfaa30f5f2ab25802c5ab3
Shreyahere <42673846+Shreyahere@users.noreply.github.com>
2019-11-19 19:17:08 +0530

    Update dbconnection.js

--- dbconnection.js
+++ dbconnection.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: "database.cibnkadocg8q.us-east-2.rds.amazonaws.com",
-    port: "3306",
-    user: "aws_db",
-    password: "KuchBhi-123",
-    database: 'testnode'
+    host: "***",
+    port: "****",
+    user: "****",
+    password: "****",
+    database: '***'
 });
 
 con.connect(function(err) {
